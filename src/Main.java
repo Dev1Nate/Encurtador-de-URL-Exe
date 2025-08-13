e1b415a0be47697a37b210fa1a55cd8ec36a2f4a
Dev1Nate <conserdavi@gmail.com>
2025-08-13 17:29:58 -0300

    adicao do try catch

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        ProcessBuilder ps = new ProcessBuilder("python", ".\\src\\python\\shortener.py", "https://www.youtube.com/watch?v=z1u_so4ZQ-w");
-        ps.redirectErrorStream(true);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Copie Ou Digite Sua URL: ");
+        try {
 
-        Process process = ps.start();
+            ProcessBuilder ps = new ProcessBuilder("python", ".\\src\\python\\shortener.py",sc.nextLine());
+            ps.redirectErrorStream(true);
+            Process process = ps.start();
+            try (BufferedReader ln = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line = ln.readLine();
+                System.out.println(line);
+            }
 
-        try (BufferedReader ln = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-            String line = ln.readLine();
-            System.out.println(line);
+        }catch (RuntimeException e ){
+            System.out.print("Erro Na URL:");
+            e.printStackTrace();
         }
+
+
+
+
+
+        sc.close();
 
 
     }
