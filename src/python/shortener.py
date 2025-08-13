22a2549c2f9d0da4e0e16520535f08b08ae5ee1d
Dev1Nate <conserdavi@gmail.com>
2025-08-13 17:41:24 -0300

    correcao do arquivo .py/adicao do try catch lá

--- src/python/shortener.py
+++ src/python/shortener.py
@@ -1,12 +1,15 @@
 import pyshorteners
 import sys
 
+try:
+    if len(sys.argv) <= 1:
+        print("Error In Python File")
+        sys.exit(1)
+    else:
+        url = sys.argv[1]
 
-if len(sys.argv) <= 1:
-    print("Error In Python File")
-    sys.exit(1)
-else:
-    url = sys.argv[1]
-s = pyshorteners.Shortener()
-x = s.tinyurl.short(url)
-print(x)
+    s = pyshorteners.Shortener()
+    x = s.tinyurl.short(url)
+    print(x)
+except pyshorteners.exceptions.BadURLException:
+    print(f"Erro Ao Converter A URL:{url} ")
